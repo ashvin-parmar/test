2c64bf29c569bd4d70c063f5a0dd2e410c4f80a4
Ashvin <ashwinparmar8181@gamil.com>
2024-09-19 03:08:36 +0530

    Eg2.c edited | Planning for recover commited data using commit id

--- eg2.c
+++ eg2.c
@@ -1,1 +1,7 @@
-Changed
+#include<stdio.h>
+int main()
+{
+printf("Hi Here we are discussed about how to resolve or recover deleted data.");
+printf("To Take previous deleted data from last commited id\n");
+return 0;
+}
