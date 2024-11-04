96774802e0ba1969eabd2f376bc7b42dd0264b59
Ashvin <ashwinparmar8181@gmail.com>
2024-11-04 23:46:41 +0530

    Old test files that are created in windows

--- eg2.c
+++ eg2.c
@@ -2,11 +2,6 @@
 int main()
 {
 printf("Hi Here we are discussed about how to resolve or recover deleted data.");
-
-Comethin
-Something
-something
-
 printf("To Take previous deleted data from last commited id\n");
 return 0;
 }
