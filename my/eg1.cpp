75cbfd48da64a00c28762aba50ebdcbaf195877a
Ashvin <ashwinparmar8181@gmail.com>
2025-08-12 14:26:25 +0530

    learning git reset --soft command

--- my/eg1.cpp
+++ my/eg1.cpp
@@ -10,5 +10,8 @@ auto ub=upper_bound(v.begin(),v.end(),30);
 cout<<"Lower Bound: "<<lb-v.begin()<<endl;
 cout<<"Upper Bound: "<<ub-v.begin()<<endl;
 
+
+//nothing just added something
+//git reset --soft 8bc003abaee1e2d2d6fbde8b796f97ea36d471c9
 return 0;
 }
